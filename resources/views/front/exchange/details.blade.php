807b0f940926dc8deb1a6545f5451a95fe7d50c9
lnacosta <laion.cj91@gmail.com>
2017-11-09 19:15:41 -0300

    added ask and bid orders to details in the exchange

--- resources/views/front/exchange/details.blade.php
+++ resources/views/front/exchange/details.blade.php
@@ -7,6 +7,71 @@
        <input type="hidden" id="currencyFrom" value="{{ $currencyFrom->id }}">
        <input type="hidden" id="currencyTo" value="{{ $currencyTo->id }}">
 
+       <div class="row info_container">
+           <div1 class="graphic">
+               <table id="graphic" class="display cell-border hover stripe" cellspacing="0">
+                   <thead>
+                   <tr>
+                       la concha de tu madre allboys
+                   </tr>
+                   </thead>
+               </table>
+           </div1>
+           <div class="info_currency">
+               <table id="info_currency" class="display cell-border hover stripe" cellspacing="0">
+                   <thead>
+                       <tr>
+                           <th colspan="2">{{$currencyFrom->name}} ({{$currencyFrom->alias}})</th>
+                       </tr>
+                   </thead>
+                   <tbody>
+                       <tr>
+                           <td rowspan="2">Último</td>
+                           <td class=""><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td rowspan="2">Volumen {{$currencyFrom->alias}}</td>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td rowspan="2">Precio Venta</td>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td rowspan="2">Precio Compra</td>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td rowspan="2">Máximo 24 horas</td>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td rowspan="2" >Mínimo 24 horas</td>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                       <tr>
+                           <td><strong>{{$currencyFrom->alias}}</strong></td>
+                       </tr>
+                   </tbody>
+               </table>
+           </div>
+       </div>
+
        <div class="row order_book_container">
            <div1 class="order_book_bid">
                <table id="order_book_bid" class="display cell-border hover stripe" cellspacing="0">
