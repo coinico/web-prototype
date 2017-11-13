9a069c39612dd08e341907811fb35777b890b77d
lnacosta <laion.cj91@gmail.com>
2017-11-13 03:49:01 -0300

    footer

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -178,11 +178,12 @@
    </header> <!-- end header -->
 
    @yield('main')
-
+	@if (!isHome())
    <!-- footer
    ================================================== -->
-   <footer style="background-color: #0C161F!important;">
-   <!--
+   <footer>
+
+
    	<div class="footer-main">
 
    		<div class="row">
@@ -195,7 +196,7 @@
 
 		      </div> <!-- end footer-info -->
 
-   <!--      	<div class="col-three tab-1-2 mob-1-2 site-links">
+         	<div class="col-three tab-1-2 mob-1-2 site-links">
 
 	      		<h4>@lang('Links del sitio')</h4>
 
@@ -204,11 +205,11 @@
 					<li><a href="{{ route('contacts.create') }}">@lang('Contáctanos')</a></li>
 					<li><a href="http://casatoken.io/blog/">@lang('Blog')</a></li>
 					<!--<li><a href="#">@lang('Política de Privacidad')</a></li>-->
-	   <!-- 			</ul>
+	    			</ul>
 
                  </div> <!-- end site-links -->
 
-   <!--       	<div class="col-three tab-1-2 mob-1-2 social-links">
+          	<div class="col-three tab-1-2 mob-1-2 social-links">
 
 	      		<h4>@lang('Social')</h4>
 
@@ -220,10 +221,30 @@
 
 	      	</div> <!-- end social links -->
 
-	   <!-- 	      </div> <!-- end row -->
+	    	      </div> <!-- end row -->
 
-	   <!--   	</div> <!-- end footer-main -->
+	      	</div> <!-- end footer-main -->
 
+	   <div class="footer-bottom">
+		   <div class="row">
+
+			   <div class="col-twelve">
+				   <div class="copyright">
+					   <span>© Copyright CasaToken 2017</span>
+					   <span></span>
+				   </div>
+
+				   <div id="go-top">
+					   <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon icon-arrow-up"></i></a>
+				   </div>
+			   </div>
+
+		   </div>
+	   </div> <!-- end footer-bottom -->
+
+	   @else
+
+		   <footer style="background-color: #0C161F!important;">
       <div class="footer-bottom" style="margin-top:-40px;margin-bottom:-40px;background-color: #0a131b!important">
       	<div class="row">
 
@@ -239,6 +260,7 @@
 			 </div>
 		</div>
       </div> <!-- end footer-bottom -->
+			   @endif
 
    </footer>
 
