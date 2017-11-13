3b6c0cef96282f42cbf3db51fe80b784fe3315ca
lnacosta <laion.cj91@gmail.com>
2017-11-13 18:57:47 -0300

    investing working with transaresultction creation

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -193,7 +193,7 @@
 	            <h4 style="color:#4e638a;">@lang('Acerca de este DEMO')</h4>
 
 	               <p>Esta versión es un prototipo de demostración para ilustrar algunas de las funcionalidades básicas, a modo de ejemplo del plan de desarrollo delineado en nuestro white-paper y modelo de negocios. El desarrollo de la plataforma seguirá las etapas descriptas en nuestro roadmap luego del cierre del período de contribución anunciado en la ICO.
-					   Suscríbete a nuestra lista de distribución para recibir información de nuestros avances (<a href="http://casatoken.io/#About">LINK</a>).</p>
+					   Suscríbete a nuestra lista de distribución para recibir información de nuestros avances <a href="http://casatoken.io/#About">aquí</a> .</p>
 
 		      </div> <!-- end footer-info -->
 
