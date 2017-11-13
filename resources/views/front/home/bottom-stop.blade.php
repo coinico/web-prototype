269a27061128a6ac428c46cac42cb65c7c91af7d
lnacosta <laion.cj91@gmail.com>
2017-11-13 03:15:02 -0300

    mejorada vista de props

--- resources/views/front/home/bottom-stop.blade.php
+++ resources/views/front/home/bottom-stop.blade.php
@@ -1,9 +1,9 @@
 <section id="bottom-stop" class="bottom-stop">
     <h1>
-        Acerca de este demo de Casatoken
+        Acerca de CasaToken DEMO
     </h1>
     <p>
-        Casatoken es una plataforma P2P de bienes raices que permite la tokenisación de propiedades individuales que funciona sobre la blockchain de Ethereum a través de contratos inteligentes. Esta version es un prototipo de demostración para ilustrar algunas de las funcionalidades básicas, a modo de ejemplo del plan de desarrollo delineado en nuestro whitepaper y modelo de negocio. El desarrollo de la plataforma seguira las etapas delineadas en nuestra ruta, a partir del cierre del período de contribución anunciado en la ICO.</br>Suscribete a nuestra lista de distribución para que te informemos de nuestros avances (<a href="http://casatoken.io/#About">LINK</a>).
+        CasaToken es una plataforma P2P de bienes raíces que permite la tokenización de propiedades individuales, funcionando sobre la blockchain de Ethereum y accionando a través de contratos inteligentes. Esta versión es un prototipo de demostración para ilustrar algunas de las funcionalidades básicas, a modo de ejemplo del plan de desarrollo delineado en nuestro white-paper y modelo de negocios. El desarrollo de la plataforma seguirá las etapas descriptas en nuestro roadmap luego del cierre del período de contribución anunciado en la ICO.</br>Suscríbete a nuestra lista de distribución para recibir información de nuestros avances (<a href="http://casatoken.io/#About">LINK</a>).
     </p>
 </section>
 
