f9c3ae0f870ccff132bf3908cf74ffb272eeee4f
lnacosta <laion.cj91@gmail.com>
2017-11-13 02:40:01 -0300

    mejorada vista de props

--- resources/views/front/exchange/sub_header.blade.php
+++ resources/views/front/exchange/sub_header.blade.php
@@ -8,6 +8,6 @@
         <div class ="sub_header_type">{!! $headerFor !!}</div>
         <div class ="sub_header_name">{!! $currency->crypto_currency_name." (".$currency->alias.")" !!}</div>
         <div class ="sub_header_volume">{!! number_format($currency->volume, 4, ".", "")." CTF" !!}</div>
-        <div class ="sub_header_change">{!! $currency->change_string !!}</div>
+        <div class ="sub_header_change {{$currency->change_percent > 0 ? "sub_header_change_green": ($currency->change_percent < 0 ? "sub_header_change_red" :"")}}"><img class="arrow_percent" src="/images/{{$currency->change_percent > 0 ? "up_arrow.png": ($currency->change_percent < 0 ? "down_arrow.png" : "leftandright.png")}} "> {!! $currency->change_string !!}</div>
     </div>
 </div1>
