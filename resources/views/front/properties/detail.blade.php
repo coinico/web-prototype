32d6770b797661383cdebf6fd61cc2c597f9e486
lnacosta <laion.cj91@gmail.com>
2017-11-13 01:37:14 -0300

    mejorada vista de props

--- resources/views/front/properties/detail.blade.php
+++ resources/views/front/properties/detail.blade.php
@@ -2,30 +2,49 @@
     <div class="property">
         <div class="info">
             @if( isset($property->images))
-                <div class="image" style="background-image: url('images/properties/{{$property->images}}') "></div>
+                <div class="image" style="margin:auto; background-image: url('images/properties/{{$property->images}}') "></div>
             @else
-                <div class="image" style="background-image: url('images/no-image.png') "></div>
+                <div class="image" style="left:0; right:0; background-image: url('images/no-image.png') "></div>
             @endif
 
             <div class="price">U$D {{number_format($property->value, 0, ',', '.')}}</div>
             <div style="margin-top:10px;height: 2px;background: #cbcbcb;position: absolute;content: '';left: 15px;right:15px; "></div>
-            <h3 style="margin-top:10px;padding-bottom:0;margin-bottom:-8px;">{{$property->title}}</h3>
+                <div style="padding-top:-65px; margin-top:9px; background-color: #f8f8f8">
+            <h3 style="margin-top:10px;padding-bottom:0;margin-bottom:-8px; ">{{$property->title}}</h3>
             <p2 style="display:inline-block; font-size:12px; margin-top:2px; color:dimgrey;"><img style="display:inline-block; width:13px; " class="location-img" src="/images/icon-location1.png"/> {{strlen($property->city) > 35 ? substr ($property->city, 0, 35)."..." : $property->city}}</p2>
+                </div>
             <div style="height: 2px;background: #838383;position: absolute;content: '';left: 15px;right:15px; "></div>
-            <p style="color:dimgrey; margin-top: 10px; margin-bottom: 0;line-height: 21px; font-size:14px; font-weight: lighter; text-align: justify;">{{substr ($property->description, 0, 180)}}...</p>
+            <p style="height: 150px; overflow:hidden; overflow-scrolling:auto; font-weight:100;color:#363636; margin-top: 10px; margin-bottom: 0;line-height: 21px; font-size:15px; ">
+                {{$property->description}}
+            </p>
                 @if($property->status_id == 1)
-                    <div style="margin-top: 10px; height: 2px;background: #cbcbcb;position: absolute;content: '';left: 15px;right:15px; "></div>
+                    <div style="padding-top:-65px; margin-top:9px; background-color: #f8f8f8">
+                        <div style="margin-top: 10px; height: 2px;background: #cbcbcb;position: absolute;content: '';left: 15px;right:15px; "></div>
 
-                    <p style="margin-top: 16px; display:inline-block;line-height: 21px; font-size:10px; font-weight: lighter;">
-                        <pedo style="color:black; font-size: 15px;">{{number_format($property->getVotingStatus('percentage'), 2, ',', '.')}}%</pedo></br>
-                        Estado de Votación
-                    </p>
-                    <p style="margin-top: 16px; text-align: right; display:inline-block;float:right; line-height: 21px; font-size:10px; font-weight: lighter;">
-                        <pedo style="color:black; font-size: 15px;">{{$property->getPositiveVotes()}}</pedo></br>
-                        Votos
-                    </p></br>
-                    <div style="margin-top: -28px; height: 1px;background: #cbcbcb;content: '';left: 15px;right:15px; "></div>
-                @else
+                        <p style="margin-top: 16px; display:inline-block;line-height: 21px; font-size:10px; font-weight: lighter;">
+                            <pedo style="font-weight:bold; color:#3e3e3e; font-size: 15px;">{{number_format($property->getVotingStatus('percentage'), 2, ',', '.')}}%</pedo></br>
+                            Estado de Votación
+                        </p>
+                        <p style="margin-top: 16px; text-align: right; display:inline-block;float:right; line-height: 21px; font-size:10px; font-weight: lighter;">
+                            <pedo style="font-weight:bold; color:#3e3e3e; font-size: 15px;">{{$property->getPositiveVotes()}}</pedo></br>
+                            Votos
+                        </p></br>
+                        <div style="margin-top: -28px; height: 1px;background: #cbcbcb;content: '';left: 15px;right:15px; "></div>
+                    </div>
+                @elseif($property->status_id == 4)
+                    <div style="padding-top:-65px; margin-top:9px; background-color: #f8f8f8">
+                        <div style="margin-top: 10px; height: 2px;background: #cbcbcb;position: absolute;content: '';left: 15px;right:15px; "></div>
+
+                        <p style="margin-top: 16px; display:inline-block;line-height: 21px; font-size:10px; font-weight: lighter;">
+                            <pedo style="color:black; font-size: 15px;">{{number_format($property->getTotalInvestment('percentage'), 2, ',', '.')}}%</pedo></br>
+                            Total Invertido
+                        </p>
+                        <p style="margin-top: 16px; text-align: right; display:inline-block;float:right; line-height: 21px; font-size:10px; font-weight: lighter;">
+                            <pedo style="color:black; font-size: 15px;">{{$property->getTotalInvestors()}}</pedo></br>
+                            Contribuyentes
+                        </p></br>
+                        <div style="margin-top: -28px; height: 1px;background: #cbcbcb;content: '';left: 15px;right:15px; "></div>
+                    </div>
                 @endif
         </div>
         <div class="hover">
