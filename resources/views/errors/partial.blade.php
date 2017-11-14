442b92e7d5fec57429a50e1c44872d2966cbae20
lnacosta <laion.cj91@gmail.com>
2017-11-14 11:10:08 -0300

    tutorial working

--- resources/views/errors/partial.blade.php
+++ resources/views/errors/partial.blade.php
@@ -2,6 +2,6 @@
 <p class="lead">{{ __('errors.error-' . $number . '-info') }}</p>
 @if($number != '503')
     <p class="lead">
-        <a href="{{ url('/') }}" class="btn btn-default">{{ __('Home') }}</a>
+        <a href="{{ url('/') }}" class="btn btn-default">{{ __('Inicio') }}</a>
     </p>
 @endif
